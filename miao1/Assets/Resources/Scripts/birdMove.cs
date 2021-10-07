0f5966aa7559df03494c5f4b9947bb720059fe27
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2021-10-07 19:22:04 +0800

    endscene

--- miao1/Assets/Resources/Scripts/birdMove.cs
+++ miao1/Assets/Resources/Scripts/birdMove.cs
@@ -1,22 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class birdMove : MonoBehaviour
-{
-    public GameObject bird;
-    public GameObject pathPoint;
-    public List<GameObject> points;
-    private GameObject point;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class birdMove : MonoBehaviour {
+    public float speed = 2; //[1] 物体移动速度
+    public Transform []target;  // [2] 目标
+    public float delta = 0.2f; // 误差值
+    private static int i = 0;
+
+    void Update () {
+        moveTo ();  
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void moveTo(){
+        // [3] 重新初始化目标点
+        //target [i].position = new Vector3 (target [i].position.x, target [i].position.y, 0);
+
+        // [4] 让物体朝向目标点 
+        transform.LookAt (target [i]);
+
+        // [5] 物体向前移动
+        transform.Translate (Vector3.forward * Time.deltaTime * speed);
+
+        // [6] 判断物体是否到达目标点
+        if (transform.position.x > target[i].position.x - delta 
+            && transform.position.x < target[i].position.x + delta
+            && transform.position.y > target[i].position.y - delta
+            && transform.position.y < target[i].position.y + delta)
+            i = (i+1)%target.Length;
     }
 }
