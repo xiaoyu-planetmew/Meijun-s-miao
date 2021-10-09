711747365cb5cf4001e826e1d1c2a7a62eb82c36
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2021-10-09 23:06:37 +0800

    bird

--- miao1/Assets/Resources/Scripts/birdMove.cs
+++ miao1/Assets/Resources/Scripts/birdMove.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class birdMove : MonoBehaviour {
     public int index = 0;                       //从初始位置触发
     public float speed = 0.05f;                 //移动速度
-    public Transform[] theWayPoints;            //移动目标点组
+    public Transform[] theWayPoints;          //移动目标点组
+    public Transform finalPoint;
+    private float passedTime; // default 0
+    public float intervalTime;  // set time interval
+    public bool isFlying;
+    public Item target;
+    private bool hungry = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +19,59 @@ public class birdMove : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //未达到指定的index位置，调用MoveToThePoints函数每帧继续移动
-        if (transform.position != theWayPoints[index].position)
+        for (int i = 0; i < GameManager.instance.items.Count; i++)
         {
-            Invoke( "MoveToThePoints" , 2f);  
+            
+            if(GameManager.instance.items[i] == target)
+            {
+                hungry = true;                
+            }
         }
+        if(!hungry)
+        {
+            if (transform.position != theWayPoints[index].position)
+            {
+            
+                if(transform.position.x < theWayPoints[index].position.x)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                if(transform.position.x > theWayPoints[index].position.x)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                MoveToThePoints();
+                isFlying = true;
+              
+            }
         //到了数组指定index位置,改变index值，不断循环
+            else
+            {
+                if(passedTime>intervalTime)
+                {
+                    index = ++index % theWayPoints.Length;
+                    passedTime = 0; //enter next loop
+                }
+            passedTime += Time.deltaTime;
+            isFlying = false;
+            }
+        }//未达到指定的index位置，调用MoveToThePoints函数每帧继续移动
         else
         {
-            index = ++index % theWayPoints.Length;
+            if(transform.position.x < finalPoint.position.x)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                if(transform.position.x > finalPoint.position.x)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+            Vector2 temp = Vector2.MoveTowards(transform.position, finalPoint.position, speed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().MovePosition(temp);
+            if(transform.position == finalPoint.position)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
         }
     }
     void MoveToThePoints()
---
+++ miao1/Assets/Resources/Scripts/triggerSceneJump.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class triggerSceneJump : MonoBehaviour
+{
+    public int sceneNum;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x - player.transform.position.x < 2)
+        {
+            SceneManager.LoadScene(sceneNum);
+        }
+    }
+    
+}
