47f8fb30235f37cf54cde2ff0e6a29009960a227
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2021-12-13 14:17:26 +0800

    birdani

--- miao1/Assets/Scripts/birdMove.cs
+++ miao1/Assets/Scripts/birdMove.cs
@@ -9,6 +9,7 @@ public class birdMove : MonoBehaviour {
     private float passedTime; // default 0
     public float intervalTime;  // set time interval
     public bool isFlying;
+    public bool isFlyingAni;
     public Item target;
     private bool hungry = false;
     // Start is called before the first frame update
@@ -34,43 +35,57 @@ public class birdMove : MonoBehaviour {
             
                 if(transform.position.x < theWayPoints[index].position.x)
                 {
-                    transform.localScale = new Vector3(-1, 1, 1);
+                    transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
                 }
                 if(transform.position.x > theWayPoints[index].position.x)
                 {
-                    transform.localScale = new Vector3(1, 1, 1);
+                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
                 MoveToThePoints();
                 isFlying = true;
-              
+                if(isFlying && !isFlyingAni)
+                {
+                    StartCoroutine("fly");
+                }
             }
         //到了数组指定index位置,改变index值，不断循环
             else
             {
+                if(isFlying && isFlyingAni)
+                {
+                this.GetComponent<Animator>().SetTrigger("land");
+                StopAllCoroutines();
+                }
                 if(passedTime>intervalTime)
                 {
                     index = ++index % theWayPoints.Length;
                     passedTime = 0; //enter next loop
+                    
                 }
             passedTime += Time.deltaTime;
             isFlying = false;
+            isFlyingAni = false;
             }
         }//未达到指定的index位置，调用MoveToThePoints函数每帧继续移动
         else
         {
             if(transform.position.x < finalPoint.position.x)
                 {
-                    transform.localScale = new Vector3(-1, 1, 1);
+                    transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
                 }
                 if(transform.position.x > finalPoint.position.x)
                 {
-                    transform.localScale = new Vector3(1, 1, 1);
+                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 }
             Vector2 temp = Vector2.MoveTowards(transform.position, finalPoint.position, speed * Time.deltaTime);
             GetComponent<Rigidbody2D>().MovePosition(temp);
             if(transform.position == finalPoint.position)
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                this.GetComponent<Animator>().SetTrigger("land");
+                StopAllCoroutines();
+                isFlying = false;
+                isFlyingAni = false;
             }
         }
     }
@@ -80,6 +95,13 @@ public class birdMove : MonoBehaviour {
         Vector2 temp = Vector2.MoveTowards(transform.position, theWayPoints[index].position, speed * Time.deltaTime);
         //考虑到可能有碰撞检测，所以使用刚体的移动方式
         GetComponent<Rigidbody2D>().MovePosition(temp);
-    
+        
+    }
+    IEnumerator fly()
+    {
+        this.GetComponent<Animator>().SetTrigger("lift");
+        isFlyingAni = true;
+        yield return new WaitForSeconds(0.5f);
+        this.GetComponent<Animator>().SetTrigger("fly");
     }
 }
