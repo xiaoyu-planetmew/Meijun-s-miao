cf090f18f20eb984422bccf0321a2c07948ed4cb
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-01-20 14:35:30 +0800

    1

--- miao1/Assets/Scripts/bambooAni.cs
+++ miao1/Assets/Scripts/bambooAni.cs
@@ -8,12 +8,15 @@ public class bambooAni : MonoBehaviour
 {
     public List<GameObject> bamaoAni = new List<GameObject>();
     [SerializeField] float growTime;
+    public GameObject fengshuGrow;
+    public GameObject fengshuUp;
     // Start is called before the first frame update
     void Start()
     {
         growTime = bamaoAni[0].GetComponent<SkeletonAnimation>().skeletonDataAsset.GetAnimationStateData().SkeletonData.FindAnimation("grow").Duration;
         growTime = growTime / 0.2f;
         getStart();
+        
     }
 
     // Update is called once per frame
@@ -30,6 +33,7 @@ public class bambooAni : MonoBehaviour
             
             StartCoroutine(shakeStart(obj));
         }
+        StartCoroutine(fengshu());
     }
     IEnumerator shakeStart(GameObject obj)
     {
@@ -41,5 +45,12 @@ public class bambooAni : MonoBehaviour
     {
         yield return new WaitForSeconds(10f);
         getStart();
+    }
+    IEnumerator fengshu()
+    {
+        fengshuGrow.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        fengshuGrow.SetActive(false);
+        fengshuUp.SetActive(true);
     }
 }
