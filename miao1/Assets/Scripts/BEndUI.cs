a7239cf730a31759e65c146a2855abc0d65d5d6a
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-02-25 00:03:19 +0800

    1

---
+++ miao1/Assets/Scripts/BEndUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BEndUI : MonoBehaviour
+{
+    public List<Sprite> titleList = new List<Sprite>();
+    public List<Sprite> rankList = new List<Sprite>();
+    public List<Sprite> numberList = new List<Sprite>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
