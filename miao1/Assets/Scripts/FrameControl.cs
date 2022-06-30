d61fff6b983431c136759954b94e2766962c78ae
mjzoo <87255495+mjzoo@users.noreply.github.com>
2022-07-01 02:32:37 +0900

    111

--- miao1/Assets/Scripts/FrameControl.cs
+++ miao1/Assets/Scripts/FrameControl.cs
@@ -7,7 +7,7 @@ public class FrameControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 30;   
+        Application.targetFrameRate = 60;   
     }
 
     // Update is called once per frame
