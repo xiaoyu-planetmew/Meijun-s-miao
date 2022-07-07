43565f8dadd0bd5745245bad8aee4d4a48080817
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-07-07 22:46:43 +0800

    1

--- miao1/Assets/Scripts/kaledoControl.cs
+++ miao1/Assets/Scripts/kaledoControl.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class kaledoControl : MonoBehaviour
     //public List<float> ratios = new List<float>();
     public GameObject activedRing;
     public int activedRingNum;
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,28 @@ public class kaledoControl : MonoBehaviour
                 activedRing = rings[i];
                 activedRingNum = i;
             }
+        }
+        rotateStop();
+        
+    }
+    void rotateStop()
+    {
+        for(int i=0; i<rings.Count; i++)
+        {
+            if(rings[i].GetComponent<kaleidoRotate>().draging = true)
+            {
+                return;
+                Debug.Log("r1");
+            }
+            
+            
+            if((rings[i].transform.localEulerAngles.z > 5 || rings[i].transform.localEulerAngles.z < 355))
+            {
+                return;
+                Debug.Log("r2");
+            }
+            Debug.Log("r3");
+            rings[i].transform.eulerAngles = Vector3.MoveTowards(rings[i].transform.eulerAngles, new Vector3(0, 0, 0), Time.deltaTime * speed); 
         }
     }
 }
