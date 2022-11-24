d942fdef2b39c5b07d9227687e7661896b612012
xiaoyu-planetmew <xiaoyu_planetmew@hotmail.com>
2022-11-24 23:56:26 +0800

    1

--- miao1/Assets/Scripts/languageButtonChange.cs
+++ miao1/Assets/Scripts/languageButtonChange.cs
@@ -36,5 +36,34 @@ public class languageButtonChange : MonoBehaviour
             //this.GetComponent<Text>().text = JanpanessString;
         }
         }
+        else if(GameObject.Find("GameManager2"))
+        {
+            if (GameManager2.instance.languageNum == 0)
+            {
+                this.transform.GetChild(0).gameObject.SetActive(true);
+                this.transform.GetChild(1).gameObject.SetActive(false);
+                this.transform.GetChild(2).gameObject.SetActive(false);
+                //this.GetComponent<Text>().text = JanpanessString;
+            }
+            if (GameManager2.instance.languageNum == 1)
+            {
+                this.transform.GetChild(1).gameObject.SetActive(true);
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                this.transform.GetChild(2).gameObject.SetActive(false);
+            }
+            if (GameManager2.instance.languageNum == 2)
+            {
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                this.transform.GetChild(1).gameObject.SetActive(false);
+                this.transform.GetChild(2).gameObject.SetActive(true);
+                //this.GetComponent<Text>().text = JanpanessString;
+            }
+        }
+        else
+        {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            this.transform.GetChild(1).gameObject.SetActive(false);
+            this.transform.GetChild(2).gameObject.SetActive(false);
+        }
     }
 }
