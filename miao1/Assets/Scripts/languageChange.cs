ecffe54a92216fac2be89a5bbc511a988a348a71
xiaoyu-planetmew <87222530+xiaoyu-planetmew@users.noreply.github.com>
2023-02-17 00:00:27 +0800

    1

--- miao1/Assets/Scripts/languageChange.cs
+++ miao1/Assets/Scripts/languageChange.cs
@@ -45,5 +45,32 @@ public class languageChange : MonoBehaviour
             //this.GetComponent<Text>().font = Resources.Load("Fonts/简剪纸");
         }
         }
+        if(GameObject.Find("GameManager2"))
+        {
+        if(GameManager2.instance.languageNum == 0)
+        {
+            this.GetComponent<Text>().text = JanpanessString;
+            //this.GetComponent<Text>().font = Resources.Load("Fonts/UDDigiKyokashoN-B");
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Fonts/UDDigiKyokashoN-B"));
+            this.GetComponent<Text>().font = obj.GetComponent<Text>().font;
+            DestroyImmediate(obj);
+        }
+        if(GameManager2.instance.languageNum == 1)
+        {
+            this.GetComponent<Text>().text = EnglishString;
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Fonts/UDDigiKyokashoN-B"));
+            this.GetComponent<Text>().font = obj.GetComponent<Text>().font;
+            DestroyImmediate(obj);
+            //this.GetComponent<Text>().font = Resources.Load("Fonts/UDDigiKyokashoN-B");
+        }
+        if(GameManager2.instance.languageNum == 2)
+        {
+            this.GetComponent<Text>().text = ChineseString;
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Fonts/简剪纸"));
+            this.GetComponent<Text>().font = obj.GetComponent<Text>().font;
+            DestroyImmediate(obj);
+            //this.GetComponent<Text>().font = Resources.Load("Fonts/简剪纸");
+        }
+        }
     }
 }
